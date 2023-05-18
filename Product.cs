f1e2803bcb0fb009b13ee2ad6a4b83d4054af541
Lucas Maidana <lucasgmaidana9@gmail.com>
2023-05-17 21:09:18 -0300

    'ejercicio1'

---
+++ Product.cs
@@ -0,0 +1,41 @@
+﻿
+namespace Ejercicio
+{
+    public class Product
+    {
+        public int Amount { get; set; }
+        public float Price { get; set; }
+        public string ProductName { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        public void AddProduct()
+        {
+            
+            Console.WriteLine("Insert Product Name");
+            this.ProductName = Console.ReadLine();
+
+
+            Console.WriteLine("Insert Price");
+            string strPrice = Console.ReadLine();
+            this.Price = float.Parse(strPrice);
+
+            Console.WriteLine("Insert Quantity");
+            string strAmount = Console.ReadLine();
+            this.Amount = int.Parse(strAmount);
+
+        }
+
+        public void RemoveProduct()
+        {
+            this.Amount--;
+        }
+
+    }
+
+}
+
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    namespace Ejercicio
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.AddProduct();
+            Console.WriteLine("Producto al agregarlo: "+product.ProductName+"|"+product.Price + "|" +product.Amount);
+            product.RemoveProduct();
+            product.RemoveProduct();
+            Console.WriteLine("Producto luego de quitar dos unidades: " + product.ProductName + "|" + product.Price + "|" + product.Amount);
+        }
+    }
+}
+
